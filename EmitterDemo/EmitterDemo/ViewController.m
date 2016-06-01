b6a61e629c7f906d251eaa9f1a657bde705801cf
Victor Chee <victor.qhj@gmail.com>
2016-06-01 10:45:07 +0800

    Add emitter

--- EmitterDemo/EmitterDemo/ViewController.m
+++ EmitterDemo/EmitterDemo/ViewController.m
@@ -17,6 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    CAEmitterLayer *emitterLayer = [CAEmitterLayer layer];
+    emitterLayer.frame = self.view.bounds;
+    [self.view.layer addSublayer:emitterLayer];
+    
+    emitterLayer.renderMode = kCAEmitterLayerAdditive;
+    emitterLayer.emitterPosition = self.view.center;
+    
+    CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
+    emitterCell.contents = (__bridge id _Nullable)([UIImage imageNamed:@"image"].CGImage);
+    emitterCell.birthRate = 150;
+    emitterCell.lifetime = 5;
+    emitterCell.color = [UIColor orangeColor].CGColor;
+    emitterCell.alphaSpeed = -0.4;
+    emitterCell.velocity = 50;
+    emitterCell.velocityRange = 50;
+    emitterCell.emissionRange = M_PI * 2.0;
+    
+    emitterLayer.emitterCells = @[emitterCell];
 }
 
 - (void)didReceiveMemoryWarning {
