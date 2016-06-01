c9a23f866e086df4992ec4b8bab1111655b1fee7
Victor Chee <victor.qhj@gmail.com>
2016-06-01 13:52:22 +0800

    Update emitter

--- EmitterDemo/EmitterDemo/ViewController.m
+++ EmitterDemo/EmitterDemo/ViewController.m
@@ -24,23 +24,27 @@
     
     emitterLayer.renderMode = kCAEmitterLayerAdditive;
     emitterLayer.emitterPosition = self.view.center;
-    
-    CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
-    emitterCell.contents = (__bridge id _Nullable)([UIImage imageNamed:@"image"].CGImage);
-    emitterCell.birthRate = 150;
-    emitterCell.lifetime = 5;
-    emitterCell.color = [UIColor orangeColor].CGColor;
-    emitterCell.alphaSpeed = -0.4;
-    emitterCell.velocity = 50;
-    emitterCell.velocityRange = 50;
-    emitterCell.emissionRange = M_PI * 2.0;
-    
-    emitterLayer.emitterCells = @[emitterCell];
+    emitterLayer.emitterShape = kCAEmitterLayerLine;
+//    emitterLayer.emitterSize = CGSizeMake(CGRectGetWidth(self.view.frame), 1);
+    emitterLayer.emitterCells = @[[self emitterCellWithColor:[UIColor orangeColor]], [self emitterCellWithColor:[UIColor purpleColor]]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (CAEmitterCell *)emitterCellWithColor:(UIColor *)color {
+    CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
+    emitterCell.contents = (__bridge id _Nullable)([UIImage imageNamed:@"image"].CGImage);
+    emitterCell.birthRate = 150;
+    emitterCell.lifetime = 5;
+    emitterCell.color = color.CGColor;
+    emitterCell.alphaSpeed = -0.4;
+    emitterCell.velocity = 50;
+    emitterCell.velocityRange = 50;
+    emitterCell.emissionRange = M_PI * 2.0;
+    return emitterCell;
 }
 
 @end
