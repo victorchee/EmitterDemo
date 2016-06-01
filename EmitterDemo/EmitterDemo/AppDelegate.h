5814aec68226b10b38251e8574f6dc035dee48a9
Victor Chee <victor.qhj@gmail.com>
2016-06-01 10:22:56 +0800

    Add project

---
+++ EmitterDemo/EmitterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmitterDemo
+//
+//  Created by Victor Chee on 16/6/1.
+//  Copyright © 2016年 VictorChee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmitterDemo/EmitterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmitterDemo
+//
+//  Created by Victor Chee on 16/6/1.
+//  Copyright © 2016年 VictorChee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
